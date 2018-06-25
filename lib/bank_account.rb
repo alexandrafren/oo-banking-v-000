5c0bbb49d214a35a8268e604402e99e2d89c47f7
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-25 15:09:08 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,11 @@
 class BankAccount
 
+  attr_accessor :name. :bank_account, :balance, :status
+  def initialize(name, bank_account = nil)
+    @name = name
+    @bank_account = bank_account
+    @balance = 1000
+    @status = open
+  end
+
 end
