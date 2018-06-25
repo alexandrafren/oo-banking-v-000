eaa840687b9840fa8cb399d7e352e5449f87cd17
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-25 15:39:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status = "complete"
+    if @status == "complete"
       @sender.deposit(@amount)
       @receiver.withdraw(@amount)
       @status = "reversed"
