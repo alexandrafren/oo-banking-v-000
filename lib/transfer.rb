8b60adeb4dd94bb9d435a6ab4d847d27a3521af5
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-25 15:18:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,14 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status, :bank_account
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
     @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @reciever.valid?
   end
 
 end
